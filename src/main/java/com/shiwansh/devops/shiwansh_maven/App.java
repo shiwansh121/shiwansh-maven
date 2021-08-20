fcb6f842b565c9d96903780c182fd2a669b2b7f0
Shiwansh Tiwari <shivatiwri35@gmail.com>
2021-08-20 16:43:55 +0530

    Initial checkin

---
+++ src/main/java/com/shiwansh/devops/shiwansh_maven/App.java
@@ -0,0 +1,13 @@
+package com.shiwansh.devops.shiwansh_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from shiwansh" );
+    }
+}
